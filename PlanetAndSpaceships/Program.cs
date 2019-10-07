b0e8e089bbe69d0a1d433bdbdc693d0dadd372c9
Sarah Fleming <sarah.k.fleming@gmail.com>
2019-10-07 09:45:11 -0500

    adding completed practice exercises

---
+++ PlanetAndSpaceships/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PlanetAndSpaceships
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> lastPlanets = new List<string>() { "Neptune", "Uranus" };
+
+            planetList.AddRange(lastPlanets);
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+
+            planetList.Add("Pluto");
+            foreach (string planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+
+            List<string> rockyPlanets = planetList.GetRange(0, 4);
+
+            Console.WriteLine("------ Rocky Planets ------");
+
+            foreach (string planet in rockyPlanets)
+            {
+                Console.WriteLine(planet);
+            }
+
+            planetList.Remove("Pluto");
+            Console.WriteLine("------ Planets without Pluto ------");
+
+            foreach (string planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+        }
+    }
+}
---
+++ RandomNumbers/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RandomNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> numbers = new List<int> {
+            random.Next(6),
+            random.Next(6),
+            random.Next(6),
+            random.Next(6),
+            random.Next(6),
+            random.Next(6),
+            };
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                // Determine if the current loop index is contained inside of the `numbers` list. Print a message to the console indicating whether the index is in the list.
+                if (numbers.Contains(i))
+                {
+                    Console.WriteLine($"numbers list contains {i}");
+                }
+                else
+                {
+                    Console.WriteLine($"numbers list does not contain {i}");
+                }
+            }
+        }
+    }
+}
